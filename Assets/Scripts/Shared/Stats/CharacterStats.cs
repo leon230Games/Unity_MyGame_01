f2246074245981c153bc5372c7fee152bf1c09ed
Leon <homik.lukasz@gmail.com>
2017-09-25 20:43:35 +0200

    STATS script

---
+++ Assets/Scripts/Shared/Stats/CharacterStats.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class CharacterStats : MonoBehaviour {
+
+
+    public int maxHealth = 100;
+    public int curentHealth { get; private set; }
+
+    public Stat damage;
+    public Stat armor;
+
+    public void Awake()
+    {
+        curentHealth = maxHealth;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            TakeDamage(10);
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        damage -= armor.GetValue();
+        damage = Mathf.Clamp(damage, 0, int.MaxValue);
+
+        curentHealth -= damage;
+        Debug.Log(transform.name + "takes " + damage + " damage");
+
+        if (curentHealth <= 0)
+        {
+            Die();
+        }
+
+    }
+
+    public virtual void Die()
+    {
+        Debug.Log("Died...");
+    }
+
+}
