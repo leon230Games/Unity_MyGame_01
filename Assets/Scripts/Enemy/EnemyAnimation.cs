e573938e1e1fbc602ac731aa9cde9077d12a4b2b
Unknown <leonek.h@gmail.com>
2017-09-30 11:19:03 +0200

    Enemy animation ===  zombie 1

---
+++ Assets/Scripts/Enemy/EnemyAnimation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAnimation : MonoBehaviour {
+
+    const float animationSmoothTime = 0.1f;
+
+    Animator animator;
+    NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+        animator = GetComponentInChildren<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float speedPercent = agent.velocity.magnitude / agent.speed;
+
+        animator.SetFloat("speedPercent", speedPercent, animationSmoothTime, Time.deltaTime);
+
+    }
+}
