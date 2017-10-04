6eb389f26d04f5d76fc481ac926d8ff32c1664f6
Leon <leonek.h@gmail.com>
2017-10-04 16:04:57 +0200

    Sword ============== script

---
+++ Assets/Sword.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sword : Weapon {
+
+    [SerializeField] Transform weaponPlaceholder;
+    PlayerController playerController;
+
+    public override void Start()
+    {
+        base.Start();
+        //Attach weapon to placeholder
+        transform.SetParent(weaponPlaceholder);
+        playerController = GetComponentInParent<PlayerController>();
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.name != gameConstants.playerColliderName && playerController.getIsAttacking())
+        {
+            Debug.Log("Sword hitted " + collider.name);
+        }
+        
+    }
+}
