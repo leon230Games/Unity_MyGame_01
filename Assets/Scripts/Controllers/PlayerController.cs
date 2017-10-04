978f270a6c9814520ee8c5a5a404285ff5216f1a
Unknown <leonek.h@gmail.com>
2017-10-04 14:38:33 +0200

    Player Controller ======= combat

---
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerCombat))]
+public class PlayerController : MonoBehaviour {
+
+    PlayerCombat playerCombat;
+
+    private void Start()
+    {
+        playerCombat = GetComponent<PlayerCombat>();
+    }
+
+    void Update () {
+
+        if (GameManager.instance.inputController.primaryAttack)
+        {
+            Debug.Log("Clicked left mouse button");
+            playerCombat.Attack(null);
+        }
+	}
+}
